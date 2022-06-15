45ec5b4b73300d6fafdadcda08f16b4a5d443d4c
tobicode <abismetibemu@gmail.com>
2022-06-15 23:15:51 +0100

    signin page and user login validation

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const lastName = document.getElementById("lname");
 const mail = document.getElementById("Email");
 const phoneNum = document.getElementById("phoneNo");
 const password = document.getElementById("pwd");
+const sigininEmail = document.getElementById("sigininEmail");
+const sigininpass = document.getElementById("sigininPassword");
+const sigininbutton = document.getElementById("sigininButton");
 console.log(select.value);
 const database = [];
 const addForm = (selectCh, firstName, lastName, email, phoneNum, password, ) => {
@@ -39,3 +42,17 @@ signform.onsubmit = e => {
   phoneNum.value = "";
   password.value = '';
 }
+sigininbutton.addEventListener("click", function () {
+  for (let i = 0; i < database.length; i++) {
+    let element = JSON.parse(localStorage.getItem("data")) || [];
+    if (sigininEmail == element[i].email) {
+       
+    } else {
+
+    }
+    
+    if (sigininpass == element[i].password) {
+    } else {
+    }
+  }
+})
